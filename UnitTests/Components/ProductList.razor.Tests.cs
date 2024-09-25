f078764b2ea182e2c0f6ae58da1922e2df3d09b6
koenigm-seattleu <koenigm@seattleu.edu>
2024-09-25 09:51:55 -0700

    Updating all tests to the new Assert.That format.  Fixed ProductListRaxor.Tests.cs

--- UnitTests/Components/ProductList.razor.Tests.cs
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -34,7 +34,7 @@ namespace UnitTests.Components
             var result = page.Markup;
 
             // Assert
-            Assert.AreEqual(true, result.Contains("The Quantified Cactus: An Easy Plant Soil Moisture Sensor"));
+            Assert.That(result.Contains("The Quantified Cactus: An Easy Plant Soil Moisture Sensor"), Is.EqualTo(true));
         }
     }
 }
